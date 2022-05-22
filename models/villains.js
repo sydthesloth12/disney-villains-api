3a495225c8df3d37f069858c2c255ac16b6d47be
Sydney <sydneysirois17@gmail.com>
2022-05-22 14:44:08 -0400

    all routes set up

---
+++ models/villains.js
@@ -0,0 +1,11 @@
+const villainsModel = (connection, Sequelize) => {
+  return connection.define('villains', {
+    id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+    name: { type: Sequelize.STRING },
+    movie: { type: Sequelize.STRING },
+    slug: { type: Sequelize.STRING },
+  },
+  { paranoid: true })
+}
+
+module.exports = villainsModel
