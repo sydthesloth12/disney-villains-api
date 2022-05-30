3629e823c5a90d1fc20464d5292babddf0d5594a
Sydney <sydneysirois17@gmail.com>
2022-05-30 15:04:37 -0400

    find all heroes test passing

--- test/controllers/villains.test.js
+++ test/controllers/villains.test.js
@@ -8,7 +8,37 @@ const {
   addVillain,
 } = require("../../controllers/villains");
 const { villains } = require("../../models/index");
-const { heroesList } = require("../mocks/villains");
+const { heroesList, villainsList } = require("../mocks/villains");
 
 chai.use(sinonChai);
 const { expect } = chai;
+
+describe("Testing the villains controller", () => {
+  let sandbox = sinon.createSandbox();
+  let stubFindAll = sandbox.stub(villains, "findAll");
+  let stubSend = sandbox.stub();
+  let stubStatus = sandbox.stub();
+  let stubSendStatus = sandbox.stub();
+  let response = {
+    send: stubSend,
+  };
+
+  beforeEach(() => {
+    stubStatus.returns({ send: stubSend });
+  });
+
+  afterEach(() => {
+    sandbox.reset();
+  });
+
+  describe("getAllVillains", () => {
+    it("gets all the villains from database and responds w response.send", async () => {
+      stubFindAll.returns(villainsList);
+
+      await getAllVillains({}, response);
+
+      expect(stubFindAll).to.have.callCount(1);
+      expect(stubSend).to.have.been.calledWith(villainsList);
+    });
+  });
+});
